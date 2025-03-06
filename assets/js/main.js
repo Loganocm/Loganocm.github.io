e598cfb840211156b399f916027b97a805b0efb1
Logan Miller <46401738+Loganocm@users.noreply.github.com>
2025-03-06 03:13:28 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,14 +72,13 @@
 	// Sidebar.
 		var $sidebar = $('#sidebar'),
 			$sidebar_inner = $sidebar.children('.inner');
-
 		// Inactive by default on <= large.
 			breakpoints.on('<=large', function() {
 				$sidebar.addClass('inactive');
 			});
 
 			breakpoints.on('>large', function() {
-				$sidebar.removeClass('inactive');
+				$sidebar.addClass('inactive');
 			});
 
 		// Hack: Workaround for Chrome/Android scrollbar position bug.
